89deb6e9c5907abe926b8e4f7549b36bbf98f27d
afinesami <samibenomar84@gmail.com>
2019-09-18 21:02:57 +0000

    Low-level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+nclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
