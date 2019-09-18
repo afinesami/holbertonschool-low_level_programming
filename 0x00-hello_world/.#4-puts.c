c1f892e723a631aba734f452b9ead28ab172a6a1
afinesami <samibenomar84@gmail.com>
2019-09-18 20:36:59 +0000

    Low-level programming & Algorithm

---
+++ 0x00-hello_world/.#4-puts.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.1835:1568837518
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include<studio.h>
+#include <stdio.h>
 
-int main(void)
+int main (void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-  return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  retun (0);
 }
