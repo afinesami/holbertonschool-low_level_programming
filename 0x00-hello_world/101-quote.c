1f67d64a9e7e717dc26fdb92123c06ec35fa3778
afinesami <samibenomar84@gmail.com>
2019-09-18 22:00:24 +0000

    Low-level programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
+}
