eb0d8f0554212805c893e1ba0e6fd5bef0d520a3
afinesami <53800455+afinesami@users.noreply.github.com>
2019-09-19 00:11:20 +0200

    Low-level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
  *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
