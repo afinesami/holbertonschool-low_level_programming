561cb572fabb840d9ff88506c5c5d050a444b4ad
afinesami <samibenomar84@gmail.com>
2019-09-19 13:47:52 +0000

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* printing the alphabet */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+		putchar('\n');
+
+	return (0);
+}
