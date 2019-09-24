e906273e92d31b413a039be925617919d1a10bd2
afinesami <samibenomar84@gmail.com>
2019-09-24 11:51:00 +0000

    function nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
 */
 void print_alphabet_x10(void)
 {
-		int i, a;
+	int i, a;
 
 		for (i = 0; i <= 9; i++)
-		{
+	{
 		for (a = 97; a <= 122; a++)
-		{
+	{
 		_putchar(a);
-		}
+	}
 		_putchar('\n');
-		}
+	}
 }
