49fd93bb63f2cbdd0f63544d510aa672704caf79
afinesami <samibenomar84@gmail.com>
2019-09-24 09:22:29 +0000

    low level programming

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	holberton();
-	return (0);
-}
-
-void holberton(void)
-{
-	char h[] = "Holberton\n";
 	int i;
-	for (i=0; h[i] != '\0'; i++)
-		_putchar(h[i]);
+	char p[] = "Holberton";
+		for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
+	return (0);
 }
