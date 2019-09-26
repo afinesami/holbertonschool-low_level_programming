16769bc45aa3be0499e3752ed0e1a8da9af25d12
afinesami <samibenomar84@gmail.com>
2019-09-26 21:06:27 +0000

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
-* _isdigit - function that checks for a digit (0 through 9)
-* @c: Changable text
-* Returns 1 if c is a digit Returns 0 otherwise.
+* _isdigit - function that check if it's a digit
+* @c: takes in a character/digit
+* Return: 1 if digit and 0 if anything else
 */
 int _isdigit(int c)
-{	
+{
 	if (c >= '0' && c <= '9')
 	return (1);
-	else
-return (0);
+	return (0);
 }
