f08c750569a3fd1b7f715cfa4aaded9b1ace27e6
afinesami <samibenomar84@gmail.com>
2019-10-01 12:43:27 +0000

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 /**
-* reset_to_98 -a function that takes a pointer to an int 
+* reset_to_98 -a function that takes a pointer to an int
 * @n: puntero
 * Return: (0)
 */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
