2d6e4ddfea27410ecfb534068dcb287a06b2fab6
afinesami <samibenomar84@gmail.com>
2019-10-01 20:11:42 +0000

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *puts2 - print pair values.
+ *@str: value to be evaluate.
+ *Return: not.
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
