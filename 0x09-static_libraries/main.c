f31ae29b166dd6542ae531b90060d224425129c7
afinesami <samibenomar84@gmail.com>
2019-10-13 23:00:08 +0000

    C - Static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+  _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+  return (0);
 }
