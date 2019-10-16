088b57a8209238c4867ff42a9cea27014b36035c
afinesami <samibenomar84@gmail.com>
2019-10-16 17:04:04 +0000

    C - malloc, free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars, and initializes it with a char
+ * @size: size of the array
+ * @c: char to be initialized with
+ * Return: pointer to array or null
+ **/
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+# C - malloc, free
+General
+
+    What is the difference between automatic and dynamic allocation
+    What is malloc and free and how to use them
+    Why and when use malloc
+    How to use valgrind to check for memory leak
