51b5f7305cf86bac6baf6969e8fd123b77d9ad32
afinesami <samibenomar84@gmail.com>
2019-10-17 18:56:51 +0000

    C - malloc, free

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -34,6 +34,7 @@ char **strtow(char *str)
 {
 	char **strDup;
 	int i, n, m, words;
+
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	words = word_counter(str);
@@ -42,6 +43,33 @@ char **strtow(char *str)
 	strDup = malloc(sizeof(char *) * (words + 1));
 	if (strDup == NULL)
 		return (NULL);
+	i = 0;
+	while (i < words && *str != '\0')
+	{
+		if (*str != ' ')
+		{
+			n = 0;
+			while (str[n] != ' ')
+				n++;
+			strDup[i] = malloc(sizeof(char) * (n + 1));
+			if (strDup[i] == NULL)
+			{
+				while (--i >= 0)
+					free(strDup[--i]);
+				free(strDup);
+				return (NULL);
+			}
+			m = 0;
+			while (m < n)
+			{
+				strDup[i][m] = *str;
+				m++, str++;
+			}
+			strDup[i][m] = '\0';
+			i++;
+		}
+		str++;
+	}
 	strDup[i] = '\0';
 	return (strDup);
 }
