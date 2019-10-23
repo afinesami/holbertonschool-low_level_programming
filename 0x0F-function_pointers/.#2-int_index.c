5da9467380081699a00316aad1e01168d76d39da
afinesami <samibenomar84@gmail.com>
2019-10-23 13:18:34 +0000

    Function pointers

---
+++ 0x0F-function_pointers/.#2-int_index.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.2719:1571822186
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - searches an integer
+ * @array: pointer array
+ * @size: number elements in array
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: first element index,on succes or return -1
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+
+	}
+		return (-1);
+}
