c7ca432313dc0b48749ee1a3af89427b5f918305
afinesami <samibenomar84@gmail.com>
2019-10-23 12:17:43 +0000

    Function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,8 @@
 /**
  * print_name - prints a name
  * @name: pointer to char
- * @f: pointer to function that returns null
- * Return: null
+ * @f: pointer to function that returns nothing
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
