f88e74b5ab0153071832a9b2a84c669429558534
afinesami <samibenomar84@gmail.com>
2019-10-31 22:57:21 +0000

    Singly linked lists

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -10,31 +9,29 @@
  */
 int main(void)
 {
-  list_t *head;
-  list_t *new;
-  list_t hello = {"World", 5, NULL};
-  size_t n;
+	list_t *head;
+	list_t *new;
+	list_t hello = {"World", 5, NULL};
+	size_t n;
 
-  head = &hello;
-  new = malloc(sizeof(list_t));
-  if (new == NULL)
-    {
-      printf("Error\n");
-      return (1);
-    }
-  new->str = strdup("Hello");
-  new->len = 5;
-  new->next = head;
-  head = new;
-  n = print_list(head);
-  printf("-> %lu elements\n", n);
-
-  printf("\n");
-  free(new->str);
-  new->str = NULL;
-  n = print_list(head);
-  printf("-> %lu elements\n", n);
-
-  free(new);
-  return (0);
+	head = &hello;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->str = strdup("Hello");
+	new->len = 5;
+	new->next = head;
+	head = new;
+	n = print_list(head);
+	printf("-> %lu elements\n", n);
+	printf("\n");
+	free(new->str);
+	new->str = NULL;
+	n = print_list(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (0);
 }
