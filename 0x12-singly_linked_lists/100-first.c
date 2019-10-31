917e6e16760304609abec29590b5a978cea839be
afinesami <samibenomar84@gmail.com>
2019-10-31 22:59:44 +0000

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * b - prints from init by the loader before main
+ */
+void b(void) __attribute__ ((constructor));
+void b(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
