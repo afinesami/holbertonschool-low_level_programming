401a3d406f7a54a150f45f66c927a2be09f1007e
afinesami <samibenomar84@gmail.com>
2019-11-05 22:57:01 +0000

    reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * reverse_listint - define function
+ * @head: describe argument
+ * Return: what does it return?
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
