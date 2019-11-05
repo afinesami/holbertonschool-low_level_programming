5af35de611abe016664e53dbe33b16cb36839fb0
afinesami <samibenomar84@gmail.com>
2019-11-05 22:38:16 +0000

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * sum_listint - sums all elements of a list
+ * @head: pointer to the head of the list
+ * Return: sum of all elements
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	for (sum = 0; head; sum += head->n, head = head->next)
+		;
+	return (sum);
+}
