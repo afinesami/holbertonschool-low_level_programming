633bfd96238484239099a16c861d1928ae883b35
afinesami <samibenomar84@gmail.com>
2019-11-05 23:06:13 +0000

    free listint

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * free_listint_safe - function that frees a listint_t list
+ * @h: free
+ * Return: return listint
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	h++;
+	return (0);
+}
