f5f87428451f3da354c906f558c050fe90e0f9eb
afinesami <samibenomar84@gmail.com>
2019-11-05 22:58:14 +0000

    print list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_listint_safe - define function
+ * @head:
+ * Return: what does it return?
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
