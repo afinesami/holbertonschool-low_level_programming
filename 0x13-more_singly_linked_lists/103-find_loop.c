587a50948b9d63d6f3e37c7c5a089b3776614d1b
afinesami <samibenomar84@gmail.com>
2019-11-05 23:02:29 +0000

    find loop

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * find_listint_loop - function that finds the loop in a linked list
+ * @head:
+ * Return: return loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+  head++;
+  return (0);
+}
